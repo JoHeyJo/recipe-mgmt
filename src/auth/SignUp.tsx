2b7c86faae21062af38101a8a2f2e67bcf576aac
jf <jpf0628@gmail.com>
2024-03-05 15:22:52 -0800

    added tailwind

--- src/auth/SignUp.tsx
+++ src/auth/SignUp.tsx
@@ -1,7 +1,11 @@
+//components
+import InputWithLabel from '../components/InputWithLabel';
+import Alert from '../components/common/Alert';
+//modules
 import API from '../api'
 import { newUser } from '../utils/types'
 import { ChangeEvent, useState } from 'react';
-import InputWithLabel from '../components/InputWithLabel';
+//styles
 import '../styles/SignUp.css'
 
 
@@ -32,65 +36,68 @@ function SignUp() {
       const token = await API.signUp(newUser);
       return token
     } catch (error) {
-      
+
     }
   }
 
   return (
-    <div className="SignUp-container">
-      <form onSubmit={handleSubmit} className="SignUp">
-        <div className="form-group">
-          <InputWithLabel
-            name={"First Name:"}
-            id={"firstName"}
-            type={"text"}
-            className={"SignUp-fn"}
-            handleChange={handleChange}
-            value={newUser.firstName}
-            required={true} />
-        </div>
-        <div className="form-group">
-          <InputWithLabel
-            name={"Last Name:"}
-            id={'lastName'}
-            type={'text'}
-            className={'SignUp-ln'}
-            handleChange={handleChange}
-            value={newUser.lastName}
-            required />
-        </div>
-        <div className='form-group'>
-          <InputWithLabel
-            id={'email'}
-            name={'Email:'}
-            className={'SignUp-email'}
-            type={'email'}
-            handleChange={handleChange}
-            value={newUser.email}
-            required />
-        </div>
-        <div className='form-group'>
-          <InputWithLabel
-            id={'password'}
-            name={'Password:'}
-            className={'SignUp-pw'}
-            type={'password'}
-            value={newUser.password}
-            handleChange={handleChange}
-            required />
-        </div>
-        <div className='form-group'>
-          <InputWithLabel
-            id={'user'}
-            name={'User name:'}
-            type={'text'}
-            className={'SignUp-user-input'}
-            value={newUser.userName}
-            handleChange={handleChange}
-            required />
-        </div>
-      </form>
-    </div>
+    <>
+      <div className="SignUp-container">
+        <form onSubmit={handleSubmit} className="SignUp">
+          <div className="form-group">
+            <InputWithLabel
+              name={"First Name:"}
+              id={"firstName"}
+              type={"text"}
+              className={"SignUp-fn"}
+              handleChange={handleChange}
+              value={newUser.firstName}
+              required={true} />
+          </div>
+          <div className="form-group">
+            <InputWithLabel
+              name={"Last Name:"}
+              id={'lastName'}
+              type={'text'}
+              className={'SignUp-ln'}
+              handleChange={handleChange}
+              value={newUser.lastName}
+              required />
+          </div>
+          <div className='form-group'>
+            <InputWithLabel
+              id={'email'}
+              name={'Email:'}
+              className={'SignUp-email'}
+              type={'email'}
+              handleChange={handleChange}
+              value={newUser.email}
+              required />
+          </div>
+          <div className='form-group'>
+            <InputWithLabel
+              id={'password'}
+              name={'Password:'}
+              className={'SignUp-pw'}
+              type={'password'}
+              value={newUser.password}
+              handleChange={handleChange}
+              required />
+          </div>
+          <div className='form-group'>
+            <InputWithLabel
+              id={'user'}
+              name={'User name:'}
+              type={'text'}
+              className={'SignUp-user-input'}
+              value={newUser.userName}
+              handleChange={handleChange}
+              required />
+          </div>
+        </form>
+      </div>
+      {/* <Alert /> */}
+    </>
   )
 }
 
--- src/components/common/Alert.tsx
+++ src/components/common/Alert.tsx
@@ -1,11 +1,11 @@
 import { ExclamationTriangleIcon } from '@heroicons/react/20/solid'
 
-export default function Example() {
+export default function Alert() {
   return (
     <div className="rounded-md bg-yellow-50 p-4">
       <div className="flex">
         <div className="flex-shrink-0">
-          <ExclamationTriangleIcon className="h-5 w-5 text-yellow-400" aria-hidden="true" />
+          {/* <ExclamationTriangleIcon className="h-5 w-5 text-yellow-400" aria-hidden="true" /> */}
         </div>
         <div className="ml-3">
           <h3 className="text-sm font-medium text-yellow-800">Attention needed</h3>
