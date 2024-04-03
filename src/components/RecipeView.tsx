f20fc152f35cccc23394fa7144e038384176514d
jf <jpf0628@gmail.com>
2024-04-03 15:56:06 -0700

    style recipes container

--- src/components/RecipeView.tsx
+++ src/components/RecipeView.tsx
@@ -1,5 +1,11 @@
+import "../styles/RecipeView.css"
 function RecipeView() {
-  return <div className="container mx-auto px-4 sm:px-6 lg:px-8 flex-1 bg-blue-100">Recipe View</div>
+  return (
+    <div id="RecipeView-container" className="container mx-auto px-4 sm:px-6 lg:px-8 flex-1">
+      <h2>Recipe</h2>
+      <div >Recipe View</div>
+    </div>
+  )
 }
 
 export default RecipeView
--- src/components/Recipes.tsx
+++ src/components/Recipes.tsx
@@ -1,15 +1,17 @@
+import "../styles/Recipes.css";
+
 const items = [
   { id: 1, recipe: "item 1" },
   { id: 2, recipe: "item 2" },
   { id: 3, recipe: "item 3" },
   // More items...
 ]
-
+//  border-red-500
 function Recipes() {
   return (
-    <ul role="list" className="border-2 border-red-500 divide-y divide-gray-200 flex-1 bg-blue-100">
+    <ul role="list" id="Recipes-container" className="border-2 mt-5 divide-y divide-gray-200 flex-1">
       {items.map((item) => (
-        <li key={item.id} className="border-2 border-yellow-500 py-4">
+        <li key={item.id} className="border-1 pt-6 py-4">
           {item.recipe}
         </li>
       ))}
