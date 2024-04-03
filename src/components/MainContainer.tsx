de19299dcda60712a6f92d9c9ebb05d1520597b1
jf <jpf0628@gmail.com>
2024-04-02 17:22:45 -0700

    wip: resolve issue with connecting via mobile browser

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -5,7 +5,7 @@ function MainContainer() {
   return (
     <div className="border-2 border-black-500 mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       {/* We've used 3xl here, but feel free to try other max-widths based on your needs */}
-      <div className="border-2 border-black-500 mx-auto max-w-3xl flex">
+      <div className="border-2 border-black-500 mx-auto max-w-1xl flex">
         <Recipes />
         <RecipeView />
       </div>
