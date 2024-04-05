859007bb43f3510ef99b9d03b9c5561bcdb40a2d
jf <jpf0628@gmail.com>
2024-04-05 16:07:12 -0700

    import togglePreferColorScheme

--- src/utils/togglePreferColorScheme.js
+++ src/utils/togglePreferColorScheme.js
@@ -1,4 +1,4 @@
-export default function togglePreferColorScheme() {
+export default (() => {
   // On page load or when changing themes, best to add inline in `head` to avoid FOUC
   if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
     document.documentElement.classList.add('dark')
@@ -14,6 +14,5 @@ export default function togglePreferColorScheme() {
 
   // Whenever the user explicitly chooses to respect the OS preference
   localStorage.removeItem('theme')
-}
+})()
 
-togglePreferColorScheme();
