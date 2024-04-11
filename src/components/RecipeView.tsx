90f5258b61dc0d6cfe236cd872445a4207b8bede
jf <jpf0628@gmail.com>
2024-04-11 12:02:14 -0700

    set tenative light colors for background and pages.

--- src/components/RecipeView.tsx
+++ src/components/RecipeView.tsx
@@ -1,7 +1,7 @@
 import "../styles/RecipeView.css"
 function RecipeView() {
   return (
-    <div id="RecipeView-container" className="divide-y container mx-auto px-4 sm:px-6 lg:px-8 flex-1">
+    <div id="RecipeView-container" className="divide-y border-2 border-blue-900 container mx-auto px-4 sm:px-6 lg:px-8 flex-1">
       <h2>Recipe</h2>
       <p >Recipe View</p>
     </div>
