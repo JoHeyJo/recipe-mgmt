c9bd2b5e1d6ed6c81c1522136e863a615bdd425e
jf <jpf0628@gmail.com>
2024-04-11 12:38:52 -0700

    wip: backend logic to handle adding recipe

--- src/components/ToggleColorScheme.tsx
+++ src/components/ToggleColorScheme.tsx
@@ -2,7 +2,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLightbulb } from '@fortawesome/free-solid-svg-icons'
 
 function ToggleColorScheme() {
-
   function toggleColorScheme() {
     document.documentElement.classList.toggle('dark');
     console.log("toggled")
