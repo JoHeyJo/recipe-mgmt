cfb6f56c49067626a8c7a9944f45126875f9e8a2
jf <jpf0628@gmail.com>
2024-04-13 15:47:39 -0700

    adding npm install @tailwindcss/forms

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -36,7 +36,7 @@ function Modal({ setShowing, isOpen}: ModalProp) {
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
               <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
-                <div>
+                {/* <div>
                   <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
                     <CheckIcon className="h-6 w-6 text-green-600" aria-hidden="true" />
                   </div>
@@ -50,7 +50,8 @@ function Modal({ setShowing, isOpen}: ModalProp) {
                       </p>
                     </div>
                   </div>
-                </div>
+                </div> */}
+                
                 <div className="mt-5 sm:mt-6">
                   <button
                     type="button"
