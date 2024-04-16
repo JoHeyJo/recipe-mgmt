fb16c3378db07e8a1d3b64b2573ed51859e448d7
jf <jpf0628@gmail.com>
2024-04-16 16:47:19 -0700

    abstract DropDownWithSearch

--- src/components/common/DropDownWithSearch.tsx
+++ src/components/common/DropDownWithSearch.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
 import { Combobox } from '@headlessui/react'
 
-const people: { id: number, name: string }[] = [
+const options: { id: number, name: string }[] = [
   { id: 1, name: 'Leslie Alexander' },
   { id: 1, name: 'Leslie Alexander' },
   { id: 1, name: 'Leslie Alexander' },
@@ -20,13 +20,13 @@ function classNames(...classes: any) {
 
 function DropDownWithSearch() {
   const [query, setQuery] = useState('')
-  const [selected, setSelected] = useState(null)
+  const [selected, setSelected] = useState("")
 
-  const filteredPeople =
+  const filteredOptions =
     query === ''
-      ? people
-      : people.filter((person) => {
-        return person.name.toLowerCase().includes(query.toLowerCase())
+      ? options
+      : options.filter((option) => {
+        return option.name.toLowerCase().includes(query.toLowerCase())
       })
 
   return (
@@ -36,18 +36,18 @@ function DropDownWithSearch() {
         <Combobox.Input
           className="w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-10 text-gray-900 shadow-sm ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           onChange={(event) => setQuery(event.target.value)}
-          displayValue={(select:any) => select?.name}
+          displayValue={(option:any) => option?.name}
         />
         <Combobox.Button className="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
           <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
         </Combobox.Button>
 
-        {filteredPeople.length > 0 && (
+        {filteredOptions.length > 0 && (
           <Combobox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-            {filteredPeople.map((person) => (
+            {filteredOptions.map((option) => (
               <Combobox.Option
-                key={person.id}
-                value={person}
+                key={option.id}
+                value={option}
                 className={({ active }) =>
                   classNames(
                     'relative cursor-default select-none py-2 pl-3 pr-9',
@@ -57,7 +57,7 @@ function DropDownWithSearch() {
               >
                 {({ active, selected }) => (
                   <>
-                    <span className={classNames('block truncate', selected && 'font-semibold')}>{person.name}</span>
+                    <span className={classNames('block truncate', selected && 'font-semibold')}>{option.name}</span>
 
                     {selected && (
                       <span
