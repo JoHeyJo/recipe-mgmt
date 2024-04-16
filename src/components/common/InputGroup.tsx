ed062e2bbb6987a3b254e72d03101a2b85307905
jf <jpf0628@gmail.com>
2024-04-16 16:18:42 -0700

    InputGroups styling

--- src/components/common/InputGroup.tsx
+++ src/components/common/InputGroup.tsx
@@ -3,9 +3,10 @@ import InputWithLabelTW from './InputWithLabelTW';
 function InputGroup() {
   return (
     <div>
-      <div className="d-flex-col mt-2 block w-full rounded-md border-0 p-5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+      <div className="flex space-x-2">
         <DropDownWithSearch />
-        <InputWithLabelTW type={"text"} name={"ingredient"} id={"ingredient"} title={"Ingredient"} />
+        <DropDownWithSearch />
+        <DropDownWithSearch />
       </div>
     </div>
   )
--- src/components/common/InputWithLabelTW.tsx
+++ src/components/common/InputWithLabelTW.tsx
@@ -15,13 +15,13 @@ function InputWithLabelTW({type, name, id, title}: InputWithLabelTWProps) {
       <div className="relative mt-2 rounded-md shadow-sm">
         <div className="absolute inset-y-0 left-0 flex items-center">
           <label htmlFor="country" className="sr-only">
-            <input
-              type="text"
-              name="phone-number"
-              id="phone-number"
-              className="block w-full rounded-md border-0 py-1.5 pl-16 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              placeholder="+1 (555) 987-6543"
-            />
+                    <input
+          type="text"
+          name="phone-number"
+          id="phone-number"
+          className="block w-full rounded-md border-0 py-1.5 pl-16 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          placeholder="+1 (555) 987-6543"
+        />
           </label>
           <select
             id="country"
