c170c5ce15f20fcbad16b47b25ec1ea3b7e30d72
jf <jpf0628@gmail.com>
2024-07-17 21:49:39 -0700

    IngredientInputGroup can now process ingredients in state WIP: inputs that don't exists are immdiately deleted when input box is changed

--- src/components/common/DropDownWithSearch.tsx
+++ src/components/common/DropDownWithSearch.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
 import { Combobox } from '@headlessui/react'
 
@@ -11,6 +11,7 @@ type DropDownWithSearchProp = {
   name: string | number;
   handleChange: (event: any) => void;
   options: { id: number, name: string }[];
+  // onSelect: (option: DropdownOption) => void;
 }
 
 /** Combobox component - ring is removed 
@@ -29,16 +30,30 @@ function DropDownWithSearch({ name, handleChange, options }: DropDownWithSearchP
         return option.name.toLowerCase().includes(query.toLowerCase())
       })
 
+      // handleChange(query)
+
+  // useEffect(() => {
+  //   onSelect(selected);
+  // }, [selected]);
+
   return (
-    <Combobox as="div" value={selected} onChange={setSelected}>
+    <Combobox as="div" value={selected} onChange={(value)=>{
+      setQuery('')
+      setSelected(value)
+      // handleChange(value)
+    }}>
       {/* <Combobox.Label className="block text-sm font-medium leading-6 text-gray-900">Assigned to</Combobox.Label> */}
       <div className="relative">
         <Combobox.Input
           className="w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-10 text-gray-900 shadow-sm ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-          // onChange={(event) => (setQuery(event.target.value))}
-          onChange={handleChange}
+          onChange={(event) => {
+            setQuery(event.target.value)
+            handleChange(event)
+          }}
+          // onChange={handleChange}
           displayValue={(displayValue: { name: string }) => displayValue?.name}
-          // name={name as string}
+          // displayValue={(option: any) => option?.name}
+          name={name as string}
         />
         <Combobox.Button className="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
           <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
--- src/components/common/IngredientInputGroup.tsx
+++ src/components/common/IngredientInputGroup.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import DropDownWithSearch from './DropDownWithSearch';
 import { Ingredient } from '../../utils/types';
 
-const liquidsDB = [{ id: 1, name: "tequila" }, { id: 1, name: "whiskey"}]
+const liquidsDB = [{ id: 1, name: "tequila" }, { id: 2, name: "whiskey"}]
 const quantityAmountsDB = [{ id: 3, name: "1 / 3" },  { id: 4, name: '4'}]
 const quantityUnitsDB = [{ id: 5, name: "oz"}]
 
@@ -21,19 +21,21 @@ function IngredientInputGroup() {
 
   /** Handles changes to newIngredient */
   function handleChange(event: any) {
+    console.log("event.target.name",event.target.name)
+    console.log("event.target.value",event.target.value)
     const { name, value } = event.target;
-    console.log(event)
-    console.log(name, value)
+    // console.log("event",event)
+    // console.log(name, value)
     setNewIngredient(i => (
       { ...i, [name]: value }
     ))
   }
 
   return (
-    <div key={1} className="flex rounded-md border-2">
-      <DropDownWithSearch key={1} handleChange={handleChange} options={quantityAmount} name={"quantityAmount"} />
-      <DropDownWithSearch key={2} handleChange={handleChange} options={quantityUnits} name={"quantityUnit"} />
-      <DropDownWithSearch key={3} handleChange={handleChange} options={liquids} name={"name"} />
+    <div className="flex rounded-md border-2">
+      <DropDownWithSearch handleChange={handleChange} options={quantityAmount} name={"quantityAmount"} />
+      <DropDownWithSearch handleChange={handleChange} options={quantityUnits} name={"quantityUnit"} />
+      <DropDownWithSearch handleChange={handleChange} options={liquids} name={"name"} />
     </div>
   )
 }
