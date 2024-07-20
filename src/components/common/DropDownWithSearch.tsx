f504663dc982ac87a9ad1a3b4e456ffd2bfd6d96
jf <jpf0628@gmail.com>
2024-07-20 11:25:18 -0700

    added index signature with parameter type string to allow dynamic access of ingredient object

--- src/components/common/DropDownWithSearch.tsx
+++ src/components/common/DropDownWithSearch.tsx
@@ -15,7 +15,7 @@ type Option = {
 
 
 type DropDownWithSearchProp = {
-  name: string | number;
+  name: string;
   addIngredient: (event: any) => void;
   options: any[];
   // onSelect: (option: DropdownOption) => void;
@@ -66,9 +66,9 @@ function DropDownWithSearch({ name, addIngredient, options }: DropDownWithSearch
         <Combobox.Input
           className="w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-10 text-gray-900 shadow-sm ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           onChange={(event: any) => setQuery(event.target.value)}
-          // displayValue={(displayValue: { name: string }) => displayValue?.name}
-          // onBlur={() => setQuery('')}
-          // name={name as string}
+          displayValue={(displayValue: {[key: string]: string}) => displayValue[name]}
+          onBlur={() => setQuery('')}
+          name={name as string}
         />
         <Combobox.Button className="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
           <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
