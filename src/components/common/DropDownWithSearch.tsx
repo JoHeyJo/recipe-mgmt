202089ad2781ef0fabe9c9d3e542b6e254cbc20d
jf <jpf0628@gmail.com>
2024-07-30 16:36:12 -0700

    wip: creating a dropdown option to handle creating new items

--- src/components/common/DropDownWithSearch.tsx
+++ src/components/common/DropDownWithSearch.tsx
@@ -51,27 +51,40 @@ function DropDownWithSearch({ name, addIngredient, options }: DropDownWithSearch
 
         //renders "+ create" option if query value doesn't exist in the dropdown options
         if (currentOptions.length < 1 && !isOptionAvailable)
-          currentOptions.push({ id: undefined, name: '+ create....' });
+          currentOptions.push({ id: undefined, [name]: '+ create...' });
         return currentOptions;
       }, []);
 
+
+  const handleChange = (option: any) => {
+    if (option.id === undefined) {
+      option[name] = query
+      console.log('create new item', option);
+      console.log('create new item', query);
+    } else {
+      console.log('existing item', option);
+    }
+    setSelected(option);
+  };
+
   useEffect(() => {
-    addIngredient(selected, name);
+    addIngredient(selected);
   }, [selected]);
 
   return (
     <Combobox as="div" value={selected}
       onChange={(value) => {
-        console.log(value)
         setQuery('')
-        setSelected(value)
+        // setSelected(value)
+        handleChange(value)
       }}>
       {/* <Combobox.Label className="block text-sm font-medium leading-6 text-gray-900">Assigned to</Combobox.Label> */}
       <div className="relative">
         <Combobox.Input
           className="w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-10 text-gray-900 shadow-sm ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           onChange={(event: any) => setQuery(event.target.value)}
-          displayValue={(displayValue: {[key: string]: string}) => displayValue?.[name]}
+          // onChange={(event: any) => console.log("selected", event.target.value)}
+          displayValue={(displayValue: { [key: string]: string }) => displayValue?.[name]}
           onBlur={() => setQuery('')}
           name={name as string}
         />
@@ -82,33 +95,64 @@ function DropDownWithSearch({ name, addIngredient, options }: DropDownWithSearch
         {filteredOptions.length > 0 && (
           <Combobox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
             {filteredOptions.map((option) => (
-              <Combobox.Option
-                key={option.id}
-                value={option}
-                className={({ active }) =>
-                  classNames(
-                    'relative cursor-default select-none py-2 pl-3 pr-9',
-                    active ? 'bg-indigo-600 text-white' : 'text-gray-900'
-                  )
-                }
-              >
-                {({ active, selected }) => (
-                  <>
-                    <span className={classNames('block truncate', selected && 'font-semibold')}>{option[name]}</span>
+              // option.id ?
+                <Combobox.Option
+                  onClick={()=>console.log("exisitng item")}
+                  key={option.id}
+                  value={option}
+                  className={({ active }) =>
+                    classNames(
+                      'relative cursor-default select-none py-2 pl-3 pr-9',
+                      active ? 'bg-indigo-600 text-white' : 'text-gray-900'
+                    )
+                  }
+                >
+                  {({ active, selected }) => (
+                    <>
+                      <span className={classNames('block truncate', selected && 'font-semibold')}>{option[name]}</span>
 
-                    {selected && (
-                      <span
-                        className={classNames(
-                          'absolute inset-y-0 right-0 flex items-center pr-4',
-                          active ? 'text-white' : 'text-indigo-600'
-                        )}
-                      >
-                        <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                      </span>
-                    )}
-                  </>
-                )}
-              </Combobox.Option>
+                      {selected && (
+                        <span
+                          className={classNames(
+                            'absolute inset-y-0 right-0 flex items-center pr-4',
+                            active ? 'text-white' : 'text-indigo-600'
+                          )}
+                        >
+                          <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                        </span>
+                      )}
+                    </>
+                  )}
+                </Combobox.Option>
+                // :
+                // <Combobox.Option
+                //   onClick={()=>console.log("create new item")}
+                //   key={option.id}
+                //   value={option}
+                //   className={({ active }) =>
+                //     classNames(
+                //       'relative cursor-default select-none py-2 pl-3 pr-9',
+                //       active ? 'bg-indigo-600 text-white' : 'text-gray-900'
+                //     )
+                //   }
+                // >
+                //   {({ active, selected }) => (
+                //     <>
+                //       <span className={classNames('block truncate', selected && 'font-semibold')}>{option[name]}</span>
+
+                //       {selected && (
+                //         <span
+                //           className={classNames(
+                //             'absolute inset-y-0 right-0 flex items-center pr-4',
+                //             active ? 'text-white' : 'text-indigo-600'
+                //           )}
+                //         >
+                //           <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                //         </span>
+                //       )}
+                //     </>
+                //   )}
+                // </Combobox.Option>
             ))}
           </Combobox.Options>
         )}
