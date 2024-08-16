39d3964d939081298342e9c20775ea3131ee38d3
jf <jpf0628@gmail.com>
2024-08-16 10:08:00 -0700

    AddRecipe views styles is larger to accomodate left and right page sections

--- src/components/requests/AddRecipe.tsx
+++ src/components/requests/AddRecipe.tsx
@@ -72,7 +72,8 @@ function AddRecipe({ setShowing, isOpen }: AddRecipe) {
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative transform rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+              {/* <Dialog.Panel className="relative transform rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6"> */}
+              <Dialog.Panel className="relative transform rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-4xl sm:p-6">
                 {/* Comment out and Image - Title - and Text */}
                 {/* <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
                     <CheckIcon className="h-6 w-6 text-green-600" aria-hidden="true" />
@@ -87,9 +88,15 @@ function AddRecipe({ setShowing, isOpen }: AddRecipe) {
                       </p>
                     </div>
                   </div> */}
-                <DropDownWithSearch name={'Ingredients'} handleOptionChange={() => {}} options={[]} handleAddOption={()=>{}} />
-                <InputWithLabel handleUpdate={updateRecipe} value={recipe.name} />
-                <IngredientsGroup handleUpdate={updateRecipe} />
+                <section id='AddRecipe-book' className='flex'>
+                  <section id='AddRecipe-left-page' className="flex-1 mr-4">
+                    <InputWithLabel handleUpdate={updateRecipe} value={recipe.name} />
+                    <IngredientsGroup handleUpdate={updateRecipe} />
+                  </section>
+                  <section id='AddRecipe-right-page' className="flex-1 ml-4">
+                    <DropDownWithSearch name={'Ingredients'} handleOptionChange={() => { }} options={[]} handleAddOption={() => { }} />
+                  </section>
+                </section>
                 <div className="mt-5 sm:mt-6">
                   <button
                     type="button"
