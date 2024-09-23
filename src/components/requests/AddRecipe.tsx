dc5dea3a58f29f1aba7902ad4c64067d7512ae28
jf <jpf0628@gmail.com>
2024-09-23 15:48:56 -0700

    refactored AddRecipe

--- src/components/requests/AddRecipe.tsx
+++ src/components/requests/AddRecipe.tsx
@@ -135,21 +135,30 @@ function AddRecipe({ setShowing, isOpen }: AddRecipe) {
         <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
           <DialogPanel
             transition
-            className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-sm sm:p-6 data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
+            className="relative h-full transform rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-4xl sm:p-6"
           >
             <div>
-              <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
+              {/* <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
                 <CheckIcon aria-hidden="true" className="h-6 w-6 text-green-600" />
-              </div>
+              </div> */}
               <div className="mt-3 text-center sm:mt-5">
-                <DialogTitle as="h3" className="text-base font-semibold leading-6 text-gray-900">
+                {/* <DialogTitle as="h3" className="text-base font-semibold leading-6 text-gray-900">
                   Payment successful
-                </DialogTitle>
-                <div className="mt-2">
+                </DialogTitle> */}
+                {/* <div className="mt-2">
                   <p className="text-sm text-gray-500">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur amet labore.
                   </p>
-                </div>
+                </div> */}
+                <section id='AddRecipe-book' className='flex h-full'>
+                  <section id='AddRecipe-left-page' className="flex-1 mr-4">
+                    <InputWithLabel handleUpdate={updateRecipe} value={recipe.name} />
+                    <IngredientsGroup handleUpdate={updateRecipe} />
+                  </section>
+                  <section id='AddRecipe-right-page' className="flex-1 ml-4 ">
+                    <InstructionsArea />
+                  </section>
+                </section>
               </div>
             </div>
             <div className="mt-5 sm:mt-6">
