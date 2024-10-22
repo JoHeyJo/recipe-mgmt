1ffd0520a0f57f0e0d7c005cb9caf0b659baab1c
jf <jpf0628@gmail.com>
2024-10-22 12:36:27 -0700

    ingredient space is correct

--- src/components/views/RecipeView.tsx
+++ src/components/views/RecipeView.tsx
@@ -4,6 +4,12 @@ type RecipeViewProps = {
   recipe: Recipe;
 }
 
+/** Renders view of entire recipe.
+ * 
+ * 
+ * RecipeContainer -> RecipeView ->
+ */
+
 function RecipeView({ recipe }: RecipeViewProps) {
   const { name, instructions, notes, ingredients } = recipe;
   console.log(ingredients)
@@ -16,13 +22,13 @@ function RecipeView({ recipe }: RecipeViewProps) {
       <div className="mt-6 border-t border-gray-100">
         <dl className="divide-y divide-gray-100">
           <div className="flex bg-white px-4 py-6 sm:gap-4 sm:px-3">
-            <dt className="text-sm font-medium leading-6 text-gray-900">Ingredients</dt>
-            <div className="flex flex-col space-y-4 ml-4">
+            <div style={{ flex: '1 1 33.33%' }} className="text-sm font-medium leading-6 text-gray-900">Ingredients</div>
+            <div style={{ flex: '2 1 66.66%' }} className="space-y-4 ml-4">
               {ingredients.map((ingredient, i) => (
-                <div key={i} id="RecipeView-ingredient" className="space-x-4 justify-space-between">
-                  <span className="text-sm leading-6 text-gray-700 sm:col-span-2">{ingredient.amount.value}</span>
-                  <span className="text-sm leading-6 text-gray-700 sm:col-span-2">{ingredient.unit.type}</span>
-                  <span className="text-sm leading-6 text-gray-700 sm:col-span-2">{ingredient.item.name}</span>
+                <div key={i} id="RecipeView-ingredient" className="flex space-x-4 ">
+                  <span className="flex-1 text-sm leading-6 text-gray-700 sm:col-span-2">{ingredient.amount.value}</span>
+                  <span className="flex-1 text-sm leading-6 text-gray-700 sm:col-span-2">{ingredient.unit.type}</span>
+                  <span className="flex-1 text-sm leading-6 text-gray-700 sm:col-span-2">{ingredient.item.name}</span>
                 </div>
               ))}
             </div>
