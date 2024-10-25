6687f35fc967feff47c1e5d52070911aee851b0b
jf <jpf0628@gmail.com>
2024-10-25 11:41:53 -0700

    correctly handling null notes so it doens't break app

--- src/components/views/NotesView.tsx
+++ src/components/views/NotesView.tsx
@@ -6,11 +6,13 @@ import { NotesViewProp } from "../../utils/props";
  * RecipeView -> NotesView
  */
 function NotesView({ notes }: NotesViewProp) {
+
   return (
     <div className="flex bg-white px-4 py-6 sm:gap-4 sm:px-3">
       <div className="basis-1/3 self-center text-sm font-medium leading-6 text-gray-900">Notes:</div>
       <ol className="basis-2/3 space-y-4 ml-4">
-        {notes.split("\n").map((notes, id) =>
+        {notes &&
+        notes.split("\n").map((notes, id) =>
         <div key={id} id="NotesView-notes" className="">
             <li className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{notes}</li>
         </div>
