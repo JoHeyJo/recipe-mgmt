025e2c0fbfbe2019b7b6d5da66194d38e60363cf
jf <jpf0628@gmail.com>
2024-10-30 10:06:15 -0700

    moving on from dark mode styles for now

--- src/components/requests/AddRecipe.tsx
+++ src/components/requests/AddRecipe.tsx
@@ -61,6 +61,7 @@ function AddRecipe({ setShowing, isOpen, handleRecipesUpdate }: AddRecipeProps) 
       <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
         <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
           <DialogPanel
+          id='AddRecipe-DialogPanel'
             transition
             className="relative h-full transform rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-4xl sm:p-6">
             <div>
