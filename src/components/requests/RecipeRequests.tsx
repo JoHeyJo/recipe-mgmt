98cb05fba85d384d35fe781ec7eaac43d3ad45ea
jf <jpf0628@gmail.com>
2024-11-26 10:10:52 -0800

    Table UI styles

--- src/components/requests/RecipeRequests.tsx
+++ src/components/requests/RecipeRequests.tsx
@@ -12,6 +12,7 @@ import { RecipeRequestsProps } from '../../utils/props';
 import NotesInput from '../ui/NotesInput';
 import { recipeTemplate as template } from "../../utils/templates";
 import { RecipeContext, RecipeContextType } from '../../context/RecipeContext';
+import { isDisabled } from '@testing-library/user-event/dist/utils';
 
 /** Processes all recipe data
  * 
@@ -142,7 +143,7 @@ function RecipeRequests({ setShowing, isOpen, handleRecipesUpdate, handleRecipeD
         <button
           type="button"
           onClick={() => editRecipe(originalRecipe, recipe)}
-          className="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 mx-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+          className="inline-flex w-full justify-center rounded-md  px-3 mx-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 bg-indigo-600">
           Update
         </button>
         <button
