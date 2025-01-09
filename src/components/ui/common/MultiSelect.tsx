3e210e624ae1cf320f12fc4b39b79a4c2b1186cf
jf <jpf0628@gmail.com>
2025-01-08 16:14:38 -0800

    Need to style book selection and implement logic switching between books:

--- src/components/ui/common/MultiSelect.tsx
+++ src/components/ui/common/MultiSelect.tsx
@@ -16,15 +16,13 @@ function MultiSelect({ books }: MultiSelectProp) {
 
       <MenuItems
         transition
-        className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
-      >
+        className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in">
         <div className="py-1">
           {books.map(book =>
             <MenuItem>
               <a
                 href="#"
-                className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 data-[focus]:outline-none"
-              >
+                className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 data-[focus]:outline-none">
                 {book.title}
               </a>
             </MenuItem>
