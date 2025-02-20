e22d67f91217472f293f0278227cadf1aac18884
jf <jpf0628@gmail.com>
2025-02-20 11:52:57 -0800

    ...so i don't need a form element...let's table this for now...

--- src/components/views/IngredientManager.tsx
+++ src/components/views/IngredientManager.tsx
@@ -82,7 +82,10 @@ function IngredientManager({ value, attribute, entity, options, handleOption, ha
         <ComboboxInput
           placeholder={entity}
           className="w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-          onChange={(event) => setQuery(event.target.value)}
+          onChange={(event) => {
+            event.preventDefault()
+            setQuery(event.target.value)
+          }}
           onBlur={() => setQuery('')}
           displayValue={(option: { [key: string]: string }) => option?.[attribute]}
         />
--- src/components/views/InstructionManager.tsx
+++ src/components/views/InstructionManager.tsx
@@ -104,7 +104,10 @@ function InstructionManager({ arrayKey, instruction, options, handleSelected, ha
           <ComboboxInput
             placeholder={instruction.instruction}
             className="w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            onChange={(event) => setQuery(event.target.value)}
+            onChange={(event) => {
+              event.preventDefault()
+              setQuery(event.target.value)
+            }}
             onBlur={() => setQuery('')}
             displayValue={(option: { instruction: string }) => option.instruction}
           />
