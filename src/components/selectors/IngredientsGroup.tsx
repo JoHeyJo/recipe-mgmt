e691edd9b447d6402da397bae34b5ef94887e251
jf <jpf0628@gmail.com>
2025-04-01 11:08:31 -0700

    installed prettier

--- src/components/selectors/IngredientsGroup.tsx
+++ src/components/selectors/IngredientsGroup.tsx
@@ -1,69 +1,81 @@
-import { useState, useEffect, useContext } from 'react';
-import { Ingredients, Ingredient } from '../../utils/types';
-import { IngredientsGroupProps } from '../../utils/props';
-import { RecipeContext } from '../../context/RecipeContext';
-import { v4 as uuidv4 } from 'uuid';
-import { defaultIngredient, recipeTemplate } from '../../utils/templates';
-import ComponentsOptionsRequests from '../requests/ComponentsOptionsRequests';
+import { useState, useEffect, useContext } from "react";
+import { Ingredients, Ingredient } from "../../utils/types";
+import { IngredientsGroupProps } from "../../utils/props";
+import { RecipeContext } from "../../context/RecipeContext";
+import { v4 as uuidv4 } from "uuid";
+import { defaultIngredient, recipeTemplate } from "../../utils/templates";
+import ComponentsOptionsRequests from "../requests/ComponentsOptionsRequests";
 
 /** Handles selected Ingredients - mutation of ingredient's array - updates parent recipe state
  * Refactor: IngredientKeys can be removed and id associated with ingredient can now be used.
  * Need to choose between using DATE or UUID
- * 
+ *
  * RecipeRequests -> IngredientsGroup -> ComponentsOptionsRequests
  */
 function IngredientsGroup({ handleRecipeUpdate }: IngredientsGroupProps) {
   const { requestAction, contextIngredients } = useContext(RecipeContext);
-  const [ingredients, setIngredients] = useState<Ingredients>(contextIngredients || recipeTemplate.ingredients);
+  const [ingredients, setIngredients] = useState<Ingredients>(
+    contextIngredients || recipeTemplate.ingredients
+  );
   const [ingredientKeys, setIngredientKeys] = useState<any>([Date.now()]); // Generate unique key on first render
 
   /** Create unique keys array needed for children components */
   useEffect(() => {
     if (requestAction === "edit") {
-      setIngredientKeys(keys => {
-        return contextIngredients.map(() => uuidv4())
-      })
+      setIngredientKeys((keys) => {
+        return contextIngredients.map(() => uuidv4());
+      });
     }
-  }, [])
+  }, []);
 
   /** Handles adding new ingredient to array of ingredients */
   function addIngredient() {
     const newKey = Date.now();
-    setIngredients(prevIngredients => [...prevIngredients, defaultIngredient])
-    setIngredientKeys(prevKeys => [...prevKeys, newKey]);
+    setIngredients((prevIngredients) => [
+      ...prevIngredients,
+      defaultIngredient,
+    ]);
+    setIngredientKeys((prevKeys) => [...prevKeys, newKey]);
   }
 
   /** Handles removing ingredient object from array of ingredients */
   function removeIngredient(index: number) {
-    setIngredients(prevIngredients => prevIngredients.filter((_, i) => i !== index));
-    setIngredientKeys(prevKeys => prevKeys.filter((_, i) => i !== index));
+    setIngredients((prevIngredients) =>
+      prevIngredients.filter((_, i) => i !== index)
+    );
+
+    setIngredientKeys((prevKeys) => prevKeys.filter((_, i) => i !== index));
   }
 
   /** Handles updates ingredient in array of ingredients */
   function updateIngredients(newIngredient: Ingredient, index: number) {
     setIngredients((prevIngredients) => {
       const newIngredients = [...prevIngredients];
-      newIngredients[index] = newIngredient
-      return newIngredients
-    })
+      newIngredients[index] = newIngredient;
+      return newIngredients;
+    });
   }
 
   const handleIngredient = {
     add: addIngredient,
     remove: removeIngredient,
-    update: updateIngredients
+    update: updateIngredients,
   };
 
   /** Updates parent state of ingredients when ingredient is added to state */
   useEffect(() => {
-    handleRecipeUpdate(ingredients, "ingredients")
-  }, [ingredients])
+    handleRecipeUpdate(ingredients, "ingredients");
+  }, [ingredients]);
 
   return (
-    <div id='IngredientsGroup-main'>
-      <ComponentsOptionsRequests ingredients={ingredients} ingredientKeys={ingredientKeys} handleIngredient={handleIngredient} />
+    <div id="IngredientsGroup-main">
+      <ComponentsOptionsRequests
+        ingredients={ingredients}
+        ingredientKeys={ingredientKeys}
+        handleIngredient={handleIngredient}
+      />
     </div>
-  )
+  );
 }
 
 export default IngredientsGroup;
