62eee0b74424e94fe66743e1d1d0eb2ce4ffa803
jf <jpf0628@gmail.com>
2025-04-11 13:23:32 -0700

    DONE:New User - Create Recipe Disable button with visual aid

--- src/components/ui/common/FaPlusButton.tsx
+++ src/components/ui/common/FaPlusButton.tsx
@@ -1,13 +1,18 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FaPlusButtonProp } from "../../../utils/props";
+import { UserContext } from "../../../context/UserContext";
+import { useContext } from "react";
 
 function FaPlusButton({ onAction }: FaPlusButtonProp) {
+  const { defaultBookId } = useContext(UserContext);
+  console.log("default book id", !!defaultBookId);
+  const isDisabled = !!defaultBookId === false ? true : false;
   return (
     <button
-      disabled
+      disabled={isDisabled}
       onClick={onAction}
-      className="font-semibold leading-7 ml-1 hover:text-gray-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-0 focus-visible:outline-zinc-600"
+      className={`font-semibold leading-7 ml-1 ${isDisabled ? "text-gray-400" : "hover:text-gray-400"} focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-0 focus-visible:outline-zinc-600`}
     >
       <FontAwesomeIcon icon={faPlus} />
     </button>
