6ccf9366d544ae074ced2133ea9bb41e56b3fbc5
jf <jpf0628@gmail.com>
2025-05-20 12:30:09 -0700

    updated yml to export ec2 connection string

--- src/api.ts
+++ src/api.ts
@@ -9,7 +9,7 @@ import {
   Instruction,
 } from "./utils/types";
 
-const BASEURL = process.env.REACT_APP_BASE_URL || process.env.EC2_CONNECTION_STRING;
+const BASEURL = process.env.REACT_APP_BASE_URL || process.env.EC2_BASE_URL;
 
 console.log("BASEURL", BASEURL)
 
