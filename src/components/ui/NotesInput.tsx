b6b11b23827a0f5d2fa44f07bd2c0338b9e65b29
jf <jpf0628@gmail.com>
2025-07-14 20:40:34 -0700

    changed bg color textarea

--- src/components/ui/NotesInput.tsx
+++ src/components/ui/NotesInput.tsx
@@ -26,14 +26,14 @@ function NotesInput({ handleUpdate }: NotesInputProps) {
       <div className="min-w-0 flex-1">
         <form action="#">
           <div className="NotesInput-text focus-within:border-gray-800">
-            <label htmlFor="notes" className="">Notes:</label>
+            <label htmlFor="notes" className=""></label>
             <textarea
               onChange={handleChange}
               id="notes"
               name="notes"
               rows={3}
-              placeholder="Serve on rocks or ice..."
-              className="block w-full resize-none rounded-xl p-2 border-2 border-gray-300 placeholder:text-gray-500 focus:border-gray-800 focus:ring-0 sm:text-sm sm:leading-6"
+              placeholder="Notes: Serve on rocks or ice..."
+              className="block w-full resize-none rounded-xl mt-3 p-2 border-2 border-gray-300 bg-accent placeholder:text-gray-500 focus:border-gray-800 focus:ring-0 sm:text-sm sm:leading-6"
               defaultValue={notes}
             />
           </div>
