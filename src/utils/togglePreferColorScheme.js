a6cce1fe3d47be10f7ccfef1744afa92cd83f123
jf <jpf0628@gmail.com>
2025-08-27 12:07:01 -0700

    housekeeping

--- src/utils/togglePreferColorScheme.js
+++ src/utils/togglePreferColorScheme.js
@@ -8,18 +8,7 @@ export default (() => {
       window.matchMedia("(prefers-color-scheme: dark)").matches)
   ) {
     document.documentElement.classList.add("dark");
-
   } else {
     document.documentElement.classList.remove("dark");
   }
-  // Whenever the user explicitly chooses light mode
-  // localStorage.theme = "light";
-
-  // Whenever the user explicitly chooses dark mode
-  // localStorage.theme = "dark";
-
-  // Whenever the user explicitly chooses to respect the OS preference
-  // localStorage.removeItem("theme");
-
-  console.log("prefered color scheme=",window.matchMedia("(prefers-color-scheme: dark)").matches);
 })();
