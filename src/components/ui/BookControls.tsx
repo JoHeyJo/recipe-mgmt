be03d99ff09e55f680d2f663b0c42dbe5aec320d
jf <jpf0628@gmail.com>
2026-04-21 16:40:30 -0700

    WIP:BookControls

--- src/components/ui/BookControls.tsx
+++ src/components/ui/BookControls.tsx
@@ -1,3 +1,16 @@
+import { ReactNode } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUsers } from "@fortawesome/free-solid-svg-icons";
+import Tooltip from "../ui/common/Tooltip";
+import FaShareButton from "../ui/common/FaShareButton";
+import FaPlusButton from "../ui/common/FaPlusButton";
+
+type BookControlsProps = {
+  role: string;
+  type: string;
+  action: () => void;
+  children?: ReactNode;
+};
 /**
  * 
       /* Render edit button:
@@ -7,15 +20,36 @@
                 viewer       role = viewer, type standard
                 Share_book(copy/remove controls NO edit) 
  */
-function BookControls(role: string, type: string, action: ()=>{}){
-  const fullPrivileges = role === "owner" && type === "standard"
-  const collaborator = role === "collaborator" && type === "standard"
-  const sharedInbox = role === "owner" && type === "shared_inbox"
-  const viewOnly = role === "viewer" && type === "standard"
+function BookControls({ role, type, action }: BookControlsProps) {
+  const fullPrivileges = role === "owner" && type === "standard";
+  const collaborator = role === "collaborator" && type === "standard";
+  const sharedInbox = role === "owner" && type === "shared_inbox";
+  const viewOnly = role === "viewer" && type === "standard";
+
+  const renderControl = {
+    share: <FaShareButton handleClick={action} />,
+    blockShare: (
+      <Tooltip content="Only book owner can share" side="top">
+        <FontAwesomeIcon icon={faUsers} />
+      </Tooltip>
+    ),
+    addRecipe: (
+      <section className="flex [flex:0.5] justify-center">
+        <FaPlusButton onAction={action} />
+      </section>
+    ),
+  };
+
+  function chooseControl(){
+    if(fullPrivileges) return renderControl.share
+    if(collaborator || sharedInbox || viewOnly) return renderControl.blockShare
+  }
+
+
   return (
-        <section>
-          {/* {role === "owner" && <FontAwesomeIcon icon={faPenToSquare} />} */}
-        </section>
-  )
+    <section>
+      {chooseControl()}
+    </section>
+  );
 }
-export default BookControls
+export default BookControls;
