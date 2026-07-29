3f0307fe3a6cdcbd5dab5a6b42b443665ab16b0f
jf <jpf0628@gmail.com>
2026-07-28 20:49:48 -0700

    INIT: Invite Component that allowers ADMIN to invite testers

--- src/components/requests/Invite.tsx
+++ src/components/requests/Invite.tsx
@@ -1,13 +1,35 @@
+import { useState } from "react";
 import InputWithLabelForm from "../views/InputWithLabelForm";
 import { PillButtonSubmit } from "../ui/PillButtonSubmit";
 
+/** Invite form component - renders  Pop Out UI that allows ADMIN to invite testers*/
 function Invite() {
+  const [tester, setTester] = useState("");
 
   function handleSubmit(){
 
   }
+  
+  function handleChange(){
 
-  return ( <></>)
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      {/* <div>{`Who would you like to share this ${action === "shareBook" ? "book" : "recipe"} with?`}</div> */}
+      <InputWithLabelForm
+        type={"user-name"}
+        name={"User Name:"}
+        id={"user-name"}
+        className={"user-name"}
+        handleChange={handleChange}
+        value={user}
+        required={true}
+        styles={"px-2 border-2 border-solid"}
+      />
+      <PillButtonSubmit action={"share"} />
+    </form>
+  );
 }
 
 export default Invite;
