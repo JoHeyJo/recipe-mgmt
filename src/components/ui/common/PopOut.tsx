3d8fb0caa22f83e805e6022ecaaae340ce61b38a
jf <jpf0628@gmail.com>
2026-07-30 10:37:07 -0700

    FIXED: Dialog not closing on background click

--- src/components/ui/common/PopOut.tsx
+++ src/components/ui/common/PopOut.tsx
@@ -1,4 +1,4 @@
-import { Dialog, DialogBackdrop } from "@headlessui/react";
+import { Dialog, DialogBackdrop, DialogPanel } from "@headlessui/react";
 import { PopOutProps } from "../../../utils/props";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
@@ -22,6 +22,7 @@ function PopOut({
    * Close PopOut component while calling parent close logic
    * */
   function handleClose() {
+    console.log("close")
     onCloseDialog();
     setTimeout(() => {
       onResetMessage();
@@ -29,29 +30,34 @@ function PopOut({
   }
 
   return (
-    <Dialog open={isDialogOpen} onClose={handleClose} className="relative z-10">
-      <DialogBackdrop
-        transition
-        className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"
-      />
-      <div className="fixed h-full inset-0 z-10 w-screen">
-        <div className="flex h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-          <div
-            id="alert-border-1"
-            className="flex items-center p-4 mb-4 text-blue-800 border-t-4 border-blue-300 bg-blue-50 dark:text-blue-400 dark:bg-gray-800 dark:border-blue-800"
-            role="alert"
-          >
-            {message ? message : children}
-            <button
-              onClick={handleClose}
-              type="button"
-              className="ms-auto -mx-1.5 -my-1.5 bg-blue-50 text-blue-500 rounded-lg focus:ring-2 focus:ring-blue-400 p-1.5 hover:bg-blue-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-blue-400 dark:hover:bg-gray-700"
-              aria-label="Close"
-            >
-              <FontAwesomeIcon icon={faXmark} />
-            </button>
+    <Dialog
+      open={isDialogOpen}
+      onClose={handleClose}
+      className="relative z-10"
+    >
+      <DialogBackdrop transition className="fixed inset-0 bg-black/30" />
+      <div className="fixed inset-0 flex items-center ">
+        <DialogPanel transition className="transform">
+          <div className="fixed h-full inset-0 z-10 w-screen">
+            <div className="flex h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+              <div
+                id="alert-border-1"
+                className="flex items-center p-4 mb-4 text-blue-800 border-t-4 border-blue-300 bg-blue-50 dark:text-blue-400 dark:bg-gray-800 dark:border-blue-800"
+                role="alert"
+              >
+                {message ? message : children}
+                <button
+                  onClick={handleClose}
+                  type="button"
+                  className="ms-auto -mx-1.5 -my-1.5 bg-blue-50 text-blue-500 rounded-lg focus:ring-2 focus:ring-blue-400 p-1.5 hover:bg-blue-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-blue-400 dark:hover:bg-gray-700"
+                  aria-label="Close"
+                >
+                  <FontAwesomeIcon icon={faXmark} />
+                </button>
+              </div>
+            </div>
           </div>
-        </div>
+        </DialogPanel>
       </div>
     </Dialog>
   );
