f66d37a7d7cfad859c9b27abb98f25e567626ad5
jf <jpf0628@gmail.com>
2026-08-11 16:53:17 -0700

    WIP: UserBookDataRequest how should requests and data flow? UBDR component handles requests - should data local to RecipeRequests and then CoponentsOptionsRequests and InstructionsRequests can consume data through context?

--- src/components/requests/UserBookDataRequest.tsx
+++ src/components/requests/UserBookDataRequest.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import API from "../../api";
 import { errorHandling } from "../../utils/ErrorHandling";
-imoprt errorHandling
 
 /** Request all of a users's instructions and ingredients OR all instructions and
  * ingredients corresponding to a book
@@ -9,14 +8,34 @@ imoprt errorHandling
 function UserBookDataRequest() {
   const [ingredients, setIngredients] = useState([]);
   const [instructions, setInstructions] = useState([]);
+  const [error, setError] = useState("");
 
+  /** Fetch instructions and ingredients associated to User */
   async function requestUserData() {
     try {
       const res = await API.getUserData();
       setIngredients(res.ingredients);
       setInstructions(res.instructions);
     } catch (error: any) {
-      const message = errorHandling("UserBookDataRequest - requestUserData", error);
+      const message = errorHandling(
+        "UserBookDataRequest - requestUserData",
+        error,
+      );
+      setError(message);
+    }
+  }
+
+  /** Fetch instructions and ingredients associated to Book */
+  async function requestBookData() {
+    try {
+      const res = await API.getUserData();
+      setIngredients(res.ingredients);
+      setInstructions(res.instructions);
+    } catch (error: any) {
+      const message = errorHandling(
+        "UserBookDataRequest - requestBookData",
+        error,
+      );
       setError(message);
     }
   }
