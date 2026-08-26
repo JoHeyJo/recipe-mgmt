37ce8657679adc47fb151036d71ea82bb86749e2
jf <jpf0628@gmail.com>
2026-08-26 16:25:31 -0700

    DONE: RecipeMove compoenent holds dropdown template - WIP: integrate functionallity

--- src/components/requests/RecipeMove.tsx
+++ src/components/requests/RecipeMove.tsx
@@ -1,5 +1,30 @@
-function RecipeMove(){
+import { Dropdown } from "react-bootstrap";
+import PopOut from "../ui/common/PopOut";
+
+function RecipeMove({ action, isDialogOpen, onCloseDialogPanel }) {
+
   return (
-    <></>
-  )
+    <PopOut isDialogOpen={isDialogOpen} onCloseDialog={onCloseDialogPanel}>
+      <div className="dropdown">
+        <button
+          className="dropdown-btn"
+          aria-haspopup="listbox"
+          aria-expanded="false"
+        >
+          Select an option
+        </button>
+        <ul className="dropdown-menu" role="listbox">
+          <li role="option" data-value="profile">
+            Profile
+          </li>
+          <li role="option" data-value="settings">
+            Settings
+          </li>
+          <li role="option" data-value="logout">
+            Logout
+          </li>
+        </ul>
+      </div>
+    </PopOut>
+  );
 }export default RecipeMove;
